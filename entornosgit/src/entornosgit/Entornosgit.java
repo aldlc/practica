38ce1967569a2f80a1744c87f44366e8df575939
Alix <Alix@DESKTOP-HDDSJ78>
2021-02-07 19:42:39 +0100

    Numeros no repetidos

--- entornosgit/src/entornosgit/Entornosgit.java
+++ entornosgit/src/entornosgit/Entornosgit.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package entornosgit;
+
 import java.util.*;
+
 /**
  *
  * @author Alix
@@ -16,24 +18,31 @@ public class Entornosgit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner sc=new Scanner(System.in);
-        
-        int numeros[]=new int [5];
-        
-        for (int i=0;i<numeros.length;i++){
-            System.out.println("Anota un número");
-            numeros[i]=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+
+        int numeros[] = new int[10];
+        int num;
+        boolean repetido;
+
+        for (int i = 0; i < numeros.length; i++) {
+            do {
+                System.out.println("Anota un número");
+                num=sc.nextInt();
+                repetido=repetido(numeros,num);
+                if (repetido==true)
+                    System.out.println("Número repetido, anota otro");
+            }while (repetido== true);
+            numeros[i]=num;
         }
-        
+
         burbuja(numeros);
-        
+
         System.out.println("Los números ordenados de menor a mayor son:");
-        for (int i=0;i<numeros.length;i++) {
+        for (int i = 0; i < numeros.length; i++) {
             System.out.println(numeros[i]);
-        }        
+        }
     }
-    
-      
+
     public static void burbuja(int[] A) {
         int i, j, aux;
         for (i = 0; i < A.length - 1; i++) {
@@ -45,5 +54,16 @@ public class Entornosgit {
                 }
             }
         }
-}    
+    }
+
+    public static boolean repetido(int[] array, int valor) {
+        boolean repetido = false;
+        for (int i = 0; i < array.length && !repetido; i++) {
+            if (array[i] == valor) {
+                repetido = true;
+            }
+        }
+        return repetido;
+    }
+
 }
